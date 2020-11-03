d9ee72634b7f8937808e24e6fa58c0ac94a0056c
RumCake <mia.hasnutdinova@gmail.com>
2020-11-03 09:56:38 -0500

    Batman say I will catch joker

--- src/test/java/BatMan.java
+++ src/test/java/BatMan.java
@@ -2,5 +2,6 @@ public class BatMan {
 
     public static void main(String[] args) {
         System.out.println("I will catch your joker");
+        System.out.println("prnting another message");
     }
 }
